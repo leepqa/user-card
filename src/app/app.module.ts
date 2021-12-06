e3de5a1a65f078e9f8a2ef10028acfa0d3dab3ae
leepq <mikita.lipavetski@innowise-group.com>
2021-12-06 15:46:23 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { ModulesComponent } from './modules/modules.component';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './modules/home/home.component';
+import { UserCardComponent } from './modules/home/components/user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    ModulesComponent,
+    ComponentsComponent,
+    HomeComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
